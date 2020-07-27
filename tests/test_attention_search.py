f549d4cf0f86089ad32b962c2c79f2d3ed617198
crodriguez1a <crodriguez1a@gmail.com>
2020-07-27 12:39:54 -0400

    More safely map indices to values

--- tests/test_attention_search.py
+++ tests/test_attention_search.py
@@ -11,4 +11,35 @@ def test_can_compute_sorted_indices_from_weights():
     assert (indices_from_weights(w) == [2, 1, 0]).all()
 
 
+def test_can_safely_map_indices_to_values():
+
+    res = attention_search(
+        np.zeros(4).reshape((1, 4)), np.zeros(24).reshape((6, 4)), n_results=3
+    )
+    assert res is not None
+
+
+def test_can_safely_ignore_mapping_with_bad_values():
+
+    mv, i = attention_search(
+        np.zeros(4).reshape((1, 4)),
+        np.zeros(24).reshape((6, 4)),
+        ["a", "b"],
+        n_results=3,
+    )
+    assert mv == []
+    assert (i == np.array([5, 4, 3])).all()
+
+
+def test_can_safely_map_indices_to_values_good_values():
+    mv, i = attention_search(
+        np.array([np.arange(4)]),
+        np.arange(24).reshape((6, 4)),
+        ["a"] * 24,
+        n_results=3,
+    )
+    assert mv == ["a", "a", "a"]
+    assert (i == np.array([5, 4, 3])).all()
+
+
 # TODO: test all the things
